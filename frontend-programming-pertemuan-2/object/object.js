e100803505fd2329237dbb9ca8549d6c1c7e6418
taufikkursalm <127370150+taufikkursalm@users.noreply.github.com>
2023-03-09 11:30:19 +0700

    Add files via upload

---
+++ frontend-programming-pertemuan-2/object/object.js
@@ -0,0 +1,15 @@
+/**
+ * Membuat object menggunakan {}.
+ * Menyimpan nilai dengan format key : value
+ */
+const user = {
+  name: "Taufik Kursalam",
+  age: 21,
+  major: "Teknik Infaormatika",
+};
+
+/**
+ * Mengakses nilai object menggunakan key.
+ * Cara akses bisa menggunakan dot atau bracket
+ */
+console.log(user.name, user["age"]);
---
+++ frontend-programming-pertemuan-2/variable.js
@@ -0,0 +1,8 @@
+/**
+ * Membuat variable dengan keyword const.
+ * Variable const tidak dapat diubah nilainya.
+ */
+const name = "Taufik Kursalam";
+const major = "Teknik Informatika";
+
+console.log(name, major);
